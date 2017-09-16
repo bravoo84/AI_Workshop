f0426cfbde64f410a7b646e78b49c364826ed591
bravoo84 <bhavikmehta98@gmail.com>
2017-09-16 15:44:03 +0530

    intro_py_4 Type casting input

--- trialpy3.py
+++ trialpy3.py
@@ -1,10 +1,3 @@
-sp=1500
-cp=1200
-if sp<cp:
-    print("congrats")
-    print("you have made an profit",sp-cp,"bucks")
-elif sp>cp:
-    print("oops u loose")
-    print("you have made an loss", cp - sp, "bucks")
-else:
-    print("you dont make any profit or loss")
+age=int(input("enter number: "))
+print("age ",age)
+print(65-age)
